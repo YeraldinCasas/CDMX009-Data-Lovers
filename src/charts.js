a59c6064dc28ec476e39ee6792d88af27950c13a
Yeraldin Casas <60666819+YeraldinCasas@users.noreply.github.com>
2020-03-20 03:28:41 -0600

    Update charts.js

--- src/charts.js
+++ src/charts.js
@@ -1,173 +1,172 @@
-
-var ctx = document.getElementById('myChart');
+var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ['Español', 'Inglés', 'Chino'],
-        datasets: [{
-            label: '# of Votes',
-            data: [4, 44, 2],
-            backgroundColor: [
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-
+  type: "doughnut",
+  data: {
+    labels: ["Español", "Inglés", "Chino"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [4, 44, 2],
+        backgroundColor: [
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)"
+        ],
+        borderColor: [
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)"
+        ],
+        borderWidth: 1
+      }
+    ]
+  }
 });
-var ctx = document.getElementById('myChart-');
+var ctx = document.getElementById("myChart-");
 var myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ['Español', 'Inglés', 'Chino'],
-        datasets: [{
-            label: '# of Votes',
-            data: [4, 44, 2],
-            backgroundColor: [
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-
+  type: "doughnut",
+  data: {
+    labels: ["Español", "Inglés", "Chino"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [4, 44, 2],
+        backgroundColor: [
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)"
+        ],
+        borderColor: [
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)"
+        ],
+        borderWidth: 1
+      }
+    ]
+  }
 });
-var ctx = document.getElementById('myChart2').getContext('2d');
+var ctx = document.getElementById("myChart2").getContext("2d");
 var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['No hispano', 'Puertorriqueño', 'Centroamericano'],
-        datasets: [{
-            label: 'No pude quitar esto :v',
-            data: [46, 3, 1],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
+  type: "bar",
+  data: {
+    labels: ["No hispano", "Puertorriqueño", "Centroamericano"],
+    datasets: [
+      {
+        label: "No pude quitar esto :v",
+        data: [46, 3, 1],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)"
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)"
+        ],
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
         }
+      ]
     }
+  }
 });
-var ctx = document.getElementById('myChart2-').getContext('2d');
+var ctx = document.getElementById("myChart2-").getContext("2d");
 var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['No hispano', 'Puertorriqueño', 'Centroamericano'],
-        datasets: [{
-            label: 'No pude quitar esto :v',
-            data: [46, 3, 1],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
+  type: "bar",
+  data: {
+    labels: ["No hispano", "Puertorriqueño", "Centroamericano"],
+    datasets: [
+      {
+        label: "No pude quitar esto :v",
+        data: [46, 3, 1],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)"
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)"
+        ],
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
         }
+      ]
     }
+  }
 });
-var ctx = document.getElementById('myChart3').getContext('2d');
+var ctx = document.getElementById("myChart3").getContext("2d");
 var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'polarArea',
+  // The type of chart we want to create
+  type: "polarArea",
 
-    // The data for our dataset
-    data: {
-        labels: ['Solteros', 'Casados'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: [
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-            ],
-            borderColor: [
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-            ],
-            data: [15, 35]
-        }]
-    },
+  // The data for our dataset
+  data: {
+    labels: ["Solteros", "Casados"],
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: ["rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)"],
+        borderColor: ["rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)"],
+        data: [15, 35]
+      }
+    ]
+  },
 
-    // Configuration options go here
-    options: {}
+  // Configuration options go here
+  options: {}
 });
-var ctx = document.getElementById('myChart3-').getContext('2d');
+var ctx = document.getElementById("myChart3-").getContext("2d");
 var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'polarArea',
+  // The type of chart we want to create
+  type: "polarArea",
 
-    // The data for our dataset
-    data: {
-        labels: ['Solteros', 'Casados'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: [
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-            ],
-            borderColor: [
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-            ],
-            data: [15, 35]
-        }]
-    },
+  // The data for our dataset
+  data: {
+    labels: ["Solteros", "Casados"],
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: ["rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)"],
+        borderColor: ["rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)"],
+        data: [15, 35]
+      }
+    ]
+  },
 
-    // Configuration options go here
-    options: {}
-}); 
+  // Configuration options go here
+  options: {}
+});
